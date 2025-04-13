d38e2b0b7e6bfc41616560e16c2b24a3c48cf162
raxovile <raxovile@outlook.com>
2025-04-13 19:40:44 +0200

    add copilot into the config

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,6 +24,7 @@ require('lazy').setup({
   { import = 'plugins.dap' },
   { import = 'plugins.telescope' },
   { import = 'plugins.treesitter' },
+  { import = 'plugins.githubCopilot' },
   --  { import = 'plugins.obsidian' },
 }, {
   ui = {
